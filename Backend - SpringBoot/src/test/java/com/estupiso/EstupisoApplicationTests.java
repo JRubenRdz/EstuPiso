ae779b4acad7943f148733d54412f51802333d4b
José Rubén Rodríguez López <jrubenrdzlpz@gmail.com>
2025-03-21 10:04:55 +0100

    Proyecto en plano

---
+++ Backend - SpringBoot/src/test/java/com/estupiso/EstupisoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estupiso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstupisoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
